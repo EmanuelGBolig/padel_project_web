41632392d9d06226e7b0d9f518fd25a9a3ae8f10
Emanuel Gomez Bolig <egomezbolig@gmail.com>
2026-02-14 19:07:42 -0300

    add email timeout

--- padel_project/settings.py
+++ padel_project/settings.py
@@ -290,6 +290,7 @@ else:
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
     EMAIL_USE_SSL = False
+    EMAIL_TIMEOUT = 15 # Timeout de 15 segundos para evitar bloqueos
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     
