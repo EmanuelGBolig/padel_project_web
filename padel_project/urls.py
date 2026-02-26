7e9ba3eb7934dbfd6f73b6660bb82a37500ea4bf
Emanuel Gomez Bolig <egomezbolig@gmail.com>
2026-02-26 11:25:14 -0300

    fix(seo): resize favicon to 192x192 square for google search compliance and add favicon.ico route

--- padel_project/urls.py
+++ padel_project/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
-from django.views.generic.base import TemplateView
+from django.views.generic.base import TemplateView, RedirectView
 from .sitemaps import StaticViewSitemap, TorneoSitemap
 
 sitemaps = {
@@ -17,6 +17,7 @@ urlpatterns = [
     path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
     path('googleed7ca9e7f31e28a7.html', TemplateView.as_view(template_name="googleed7ca9e7f31e28a7.html", content_type="text/html")),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('favicon.ico', RedirectView.as_view(url=settings.STATIC_URL + 'img/favicon.ico', permanent=True)),
     # URLs de Core (Home)
     path('', include('core.urls')),
     # URLs de Accounts (Login, Logout, Registro, Perfil)
