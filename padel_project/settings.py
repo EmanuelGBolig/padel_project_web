7b9006ca89431102678d17dd2278ffee5e4bf0ee
Emanuel Gomez Bolig <egomezbolig@gmail.com>
2026-03-17 18:24:43 -0300

    Auth: Enable Google account association by email to fix login loop

--- padel_project/settings.py
+++ padel_project/settings.py
@@ -276,6 +276,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
     'https://www.googleapis.com/auth/userinfo.profile',
 ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['first_name', 'last_name', 'picture']
+SOCIAL_AUTH_ASSOCIATE_BY_EMAIL = True
 
 # --- Pipeline personalizado para Google OAuth2 ---
 SOCIAL_AUTH_PIPELINE = (
@@ -284,6 +285,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.auth_allowed',
     'social_core.pipeline.social_auth.social_user',
     'social_core.pipeline.user.get_username',
+    'social_core.pipeline.social_auth.associate_by_email',  # <--- NUEVO: Vincular por email si ya existe
     'social_core.pipeline.user.create_user',
     'social_core.pipeline.social_auth.associate_user',
     'social_core.pipeline.social_auth.load_extra_data',
